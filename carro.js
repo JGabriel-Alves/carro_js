8c7d6dc173bff87b0341103defe3c1ae32acf804
JGabrielBoss <josegabriel04sp@gmail.com>
2023-10-28 22:30:23 -0300

    Atualização pequena
    Aqui eu tirei os 2 botões e deixei somente 1, além disso, caso a Caravan quebre, o usuário não será capaz de usar o botão novamente.

--- carro.js
+++ carro.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Seu código JavaScript aqui
-    const burnoutOn = document.getElementById("turnOn");
-    const burnoutOff = document.getElementById("turnOff");
+
+    //criando as variaveis e coletando os valores delas no HTML
+    const burnoutBtn = document.getElementById("turnOnOff");
     const carro = document.getElementById("carro");
     const h1 = document.getElementById("h1");
 
+    //Função para verificar se a image3 foi chamada
     function isBroken(){
         return carro.src.indexOf("image3") > -1;
     }
 
+    //Essa função troca a imagem da Caravan e muda a tag H1 no HTML
+    //Ela liga a Caravan
     function turnOn(){
         if(!isBroken()){
             carro.src = "./img/image.png";
@@ -16,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    //Essa função "deliga" a Caravan
     function turnOff(){
         if(!isBroken()){
             carro.src = "./img/image2.png";
@@ -23,12 +27,29 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    //Essa função "quebra" a Caravan
     function broken(){
         carro.src = "./img/image3.png";
         h1.textContent = "Droga a Caravan quebrou :(";
+        burnoutBtn.disabled = true;
+        burnoutBtn.textContent = "Cabo a brincadeira";
     }
 
-    burnoutOn.addEventListener("click", turnOn);
-    burnoutOff.addEventListener("click", turnOff);
-    carro.addEventListener("dblclick", broken)
+    //Função para funcionar o botão para "desligar" e "ligar" a Caravan
+    function turnOnOff(){
+        //Verifica se o botão contem a string "Burnout ON", se sim, chama a função turnOn();
+        if(burnoutBtn.textContent == "Burnout ON"){
+            turnOn();
+            burnoutBtn.textContent = "Burnout OFF";
+        }
+        else if(burnoutBtn.textContent == "Burnout OFF"){
+            turnOff();
+            burnoutBtn.textContent = "Burnout ON";
+        }
+    }
+
+    //Adicionando os eventos do mouse
+    burnoutBtn.addEventListener("click", turnOnOff);
+    carro.addEventListener("dblclick", broken);
+    
 });
